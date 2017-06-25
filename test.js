15cb29c16a5cfcc5008d56f156ca2c7636fe4823
kohyusik <newtype26@naver.com>
2017-06-25 17:38:10 +0900

    test commit

---
+++ test.js
@@ -0,0 +1,12 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes/example');
+var http = require('http');
+var path = require('path');
+
+
+var app = express();
+app.use(express.static(path.join(__dirname, 'public')));
